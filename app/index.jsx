90faf954806a3cbc5a3333ababe29ca41079a450
Gerardo Hernández <ghernagon@gmail.com>
2016-02-21 17:44:40 -0430

    installing React and React-DOM

---
+++ app/index.jsx
@@ -0,0 +1,17 @@
+import './main.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+// var component = require('./component');
+
+// require('./main.css');
+
+// var app = document.createElement('div');
+
+// document.body.appendChild(app);
+
+// app.appendChild(component());
